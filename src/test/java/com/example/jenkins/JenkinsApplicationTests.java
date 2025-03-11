89db82d0201a02882ae55b24846a8ac6c988d86c
Saqib <89591321+Sub-Zero-Wins@users.noreply.github.com>
2025-03-11 11:27:23 +0530

    Add files via upload

---
+++ src/test/java/com/example/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.jenkins;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+		assertTrue(true);
+	}
+
+}
